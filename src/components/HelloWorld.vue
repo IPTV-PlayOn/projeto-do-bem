8a89255bc7a50a1e43ed1dd6fa45fad5a99b6ef5
IPTV PlayOn <85048488+IPTV-PlayOn@users.noreply.github.com>
2021-06-05 06:35:25 -0300

    Primeiro commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,326 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <!-- Start Header -->
+    <header id="mu-header" class="" role="banner">
+      <div class="container">
+        <nav class="navbar navbar-default mu-navbar">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button
+                type="button"
+                class="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false"
+              >
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+              <!-- Image Logo -->
+              <a class="navbar-brand" href="#"
+                ><img src="assets/images/FamiliadoBem.jpg"
+              /></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div
+              class="collapse navbar-collapse"
+              id="bs-example-navbar-collapse-1"
+            >
+              <ul class="nav navbar-nav mu-menu navbar-right">
+                <li><a href="#">Home</a></li>
+                <li><a href="#mu-about">Quem somos</a></li>
+                <li><a href="#mu-service">O que fazemos</a></li>
+                <li><a href="#mu-act">Doações</a></li>
+                <li><a href="#mu-contact">Contato</a></li>
+              </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+          </div>
+          <!-- /.container-fluid -->
+        </nav>
+      </div>
+    </header>
+    <!-- End Header -->
+
+    <!-- Start Featured Slider -->
+
+    <section id="mu-featured-slider">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="mu-featured-area">
+              <h1>Família do Bem</h1>
+
+              <!-- Start text slide -->
+              <div class="mu-featured-slider-wrapper">
+                <div class="mu-featured-slider" id="typed-strings">
+                  <span>Nosso combustível é a solidariedade.</span>
+                </div>
+                <span id="typed"></span>
+              </div>
+              <!-- Start text slide -->
+
+              <div class="mu-featured-content">
+                <a href="#mu-portfolio" class="mu-primary-btn view-my-work-btn"
+                  >Faça uma doação</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Start Featured Slider -->
+
+    <!-- Start main content -->
+
+    <main role="main">
+      <!-- Start About -->
+      <section id="mu-about">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="mu-about-area">
+                <div class="mu-about-header">
+                  <h2 class="mu-heading-title">QUEM SOMOS</h2>
+                  <span class="mu-header-dot"></span>
+                  <p>
+                    A <b>família do Bem</b> deu início no dia
+                    <b>17 de Junho de 2020</b>, após uma conversa que nosso
+                    representante teve com um colega. Seu colega enviou um vídeo
+                    de uma entrevista feita com ele, onde ele contava que estava
+                    trabalhando como motoboy e que nessa pandemia os pedidos
+                    tinham tido um aumento bem significativo (algo bastante
+                    positivo, pois estava conseguindo ajudar dentro de casa).
+                    Porém tudo resolveu mudar quando em uma de suas entregas,
+                    ele é roubado e perde sua moto. Moto essa que ele juntou
+                    dinheiro com o emprego anterior para compra-la. Perdido a
+                    moto, acabou perdendo o emprego também. E assim, estava
+                    tendo que recolher recicláveis para ajudar em casa. Isso
+                    muito comoveu o nosso representante e de imediato, ele
+                    pensou em criar um projeto social para ajudar esse rapaz. E
+                    aí nasceu a <b>Família do Bem</b>, um projeto social que
+                    busca ajudar pessoas que estão passando por dificuldades,
+                    <b
+                      >arrecadando alimentos não perecíveis e distribuindo
+                      cestas básicas para ajudar essas famílias</b
+                    >. O projeto também arrecada roupas para doações. O projeto
+                    começou no WhatsApp, onde temos o grupo de colaboradores,
+                    grupo de representantes e grupo de pessoas que necessitam da
+                    nossa colaboração.<b>
+                      O projeto se expandiu para o Instagram, onde nossa conta é
+                      para contato com as pessoas que quiserem fazer parte do
+                      grupo de colaboradores, ou que precisarem da ajuda do
+                      projeto.</b
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End About -->
+
+      <!-- Start Service -->
+      <section id="mu-service">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="mu-service-area">
+                <div class="mu-service-header">
+                  <h2 class="mu-heading-title">O QUE FAZEMOS</h2>
+                  <span class="mu-header-dot"></span>
+                  <p>
+                    Nós realizamos dois trabalhos: arrecadações de
+                    roupas(estando em boas condições para serem doadas,
+                    aceitamos de todo o coração) e também doações de alimentos
+                    não perecíveis.
+                  </p>
+                </div>
+
+                <!-- Service Single Content -->
+
+                <div class="mu-about-content">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="mu-about-content-left">
+                        <img
+                          class="mu-profile-img"
+                          src="assets/images/picPayCapa.png"
+                          alt="Profile Image"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="mu-about-content-right">
+                        <!-- Start Social Media -->
+                        <div class="mu-social-media">
+                          <h3>Nos acompanhe em nossas redes</h3>
+
+                          <!--<a href="#"><i class="fa fa-facebook"></i></a>-->
+                          <a href="#"><i class="fa fa-instagram"></i></a>
+                          <a
+                            href="https://api.whatsapp.com/send?phone=27988761587&text=Ol%C3%A1!"
+                            ><i class="fa fa-whatsapp" target="_blank"></i
+                          ></a>
+                        </div>
+
+                        <!-- Download Ressume Button -->
+                        <a href="#" class="mu-resume-btn">Faça uma doação</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- End About Us Content -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End About -->
+      <!-- End Service -->
+
+      <section id="#mu-act">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="mu-about-area">
+                <div class="mu-about-header">
+                  <h1>
+                    A Família do Bem é um projeto social formado por um grupo de
+                    colaboradores, que tem o objetivo de ajudar pessoas que
+                    estão passando por dificuldades, na situação atual. Venha
+                    colaborar com a gente.
+                  </h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End About -->
+
+      <!-- Start Contact -->
+      <section id="mu-contact">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="mu-contact-area">
+                <div class="mu-contact-header">
+                  <h2 class="mu-heading-title">CONTATO</h2>
+                  <span class="mu-header-dot"></span>
+                  <h4>
+                    Conte suas ideias, sugestões e opniões sobre nossa causa e
+                    até mesmo seja voluntário conosco
+                  </h4>
+                </div>
+
+                <!-- Start Contact Content -->
+                <div class="mu-contact-content">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="mu-contact-top">
+                        <div class="row">
+                          <div class="col-sm-4">
+                            <div class="mu-contact-top-single">
+                              <div class="mu-icon">
+                                <i class="fa fa-map-marker"></i>
+                              </div>
+                              <p>Vitória - Espírito Santo</p>
+                            </div>
+                          </div>
+                          <div class="col-sm-4">
+                            <div class="mu-contact-top-single">
+                              <div class="mu-icon">
+                                <i class="fa fa-phone"></i>
+                              </div>
+                              <p>27 988761587</p>
+                            </div>
+                          </div>
+                          <div class="col-sm-4">
+                            <div class="mu-contact-top-single">
+                              <div class="mu-icon">
+                                <i class="fa fa-envelope"></i>
+                              </div>
+                              <p>ongfamiliadobemes@gmail.com</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="col-md-12">
+                      <div class="mu-contact-bottom">
+                        <div id="form-messages"></div>
+                        <form
+                          id="ajax-contact"
+                          method="post"
+                          action="mailer.php"
+                          class="mu-contact-form"
+                        >
+                          <div class="form-group">
+                            <input
+                              type="text"
+                              class="form-control"
+                              placeholder="Nome"
+                              id="name"
+                              name="name"
+                              required
+                            />
+                          </div>
+                          <div class="form-group">
+                            <input
+                              type="email"
+                              class="form-control"
+                              placeholder="Enter Email"
+                              id="email"
+                              name="email"
+                              required
+                            />
+                          </div>
+                          <div class="form-group">
+                            <textarea
+                              class="form-control"
+                              placeholder="Message"
+                              id="message"
+                              name="message"
+                              required
+                            ></textarea>
+                          </div>
+                          <button type="submit" class="mu-resume-btn">
+                            <span>Enviar</span>
+                          </button>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- End Contact Content -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End Contact -->
+    </main>
+
+    <!-- End main content -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld"
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
